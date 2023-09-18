30bf330edaa4995d87ed08eac475d4acf022fc3c
Melissa Julia L. L <108154398+melissaJll@users.noreply.github.com>
2023-09-17 21:00:02 -0300

    Update app.js

--- exercicio2/app.js
+++ exercicio2/app.js
@@ -3,10 +3,10 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("pages/index");
+    res.render("paginas/index");
 })
 app.get("/404", (req, res) => {
-    res.render("pages/404");
+    res.render("paginas/404");
 })
 app.listen(3000, () => {
     console.log("Servidor expressJS rodando!");
